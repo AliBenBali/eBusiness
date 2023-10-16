ed3bb6abe4691faf3fc3e1e3b39ba303ebe14645
Ali Cicek <ciceka801@gmail.com>
2023-10-16 19:36:51 +0200

    vl2 fertig

---
+++ fctns.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <title>Untitled</title>
+</head>
+
+<body>
+    <?php
+    //Ausgabe mit echo
+    echo "<h1>Mein erstes PHP-Skript</h1>";
+
+    function nettoZuBrutto($netto)
+    {
+        $brutto = $netto * 1.19;
+        return round($brutto, 2);
+    }
+
+    for ($i = 1; $i <= 10; $i++) {
+        echo "Netto ", $i, " ist Brutto ", nettoZuBrutto($i), "</br> \n";
+    }
+
+    ?>
+
+</body>
+
+</html>
