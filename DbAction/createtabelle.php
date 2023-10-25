e2df03b83ccc8b807b3dff491b226a8d1fec7911
Ali Cicek <ciceka801@gmail.com>
2023-10-25 21:37:46 +0200

    tabelle code untested und grandeFinale nur kopiert

---
+++ DbAction/createtabelle.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+    <?php
+    define("servername", "localhost");
+    define("username", "username");
+    define("password", "password");
+
+    // Create connection 
+    $conn = new mysqli(servername, username, password);
+
+    // Check connection	
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    //create tabel with name and email and timestamp
+    $sql = "CREATE TABLE MyGuests (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(30) NOT NULL,
+        email VARCHAR(50),
+        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )";
+
+    //insert data into table with sql 5 times
+    $sql = "INSERT INTO MyGuests (name, email)
+    VALUES ('John', 'john@example.com');";
+    $sql .= "INSERT INTO MyGuests (name, email)
+    VALUES ('Mary', 'mary@example.com');";
+    $sql .= "INSERT INTO MyGuests (name, email)
+    VALUES ('Julie', 'julie@example.com');";
+    $sql .= "INSERT INTO MyGuests (name, email)
+    VALUES ('Donald', 'donald@example.com');";
+
+    //show data from table in html table
+    $sql = "SELECT id, name, email FROM MyGuests";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
+    /*     //delete donald
+    $sql = "DELETE FROM MyGuests WHERE id=4";
+    //update john to johnny
+    $sql = "UPDATE MyGuests SET name='Johnny' WHERE id=1";
+    //show data from table in html table
+    $sql = "SELECT id, name, email FROM MyGuests";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    } */
+
+    //vscode comment block: shift + alt + a
+
+    //close connection
+    mysqli_close($conn);
+    ?>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+        <label for="">Name: </label>
+        <input type="text" name="name" id="name">
+        <br>
+        <label for="">Email: </label>
+        <input type="email" name="email" id="email">
+        <br>
+        <input type="submit" name="senden">
+    </form>
+
+</body>
+
+</html>
---
+++ DbAction/infoGrandeFinal.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <meta name="author" content="AliBenBali">
+    <meta name="Beschreibung" content="Bruh">
+    <meta charset="UTF-8">
+    <style>
+        .error {
+            color: #FF0000;
+        }
+    </style>
+    <title>Infomaterial</title>
+</head>
+
+<body>
+    <?php
+    // define variables and set to empty values
+    $nameErr = $strasseErr = $plzErr = $ortErr = "";
+    $info = $name = $strasse = $plz = $ort = $jahreszeit = $wuensche = "";
+
+    //Check if values are not empty and if they are valid
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $info = $_POST["info"];
+        if (empty($_POST["name"])) {
+            $nameErr = "Name is required";
+        } else {
+            $name = test_input($_POST["name"]);
+        }
+        if (empty($_POST["strasse"])) {
+            $strasseErr = "Strasse is required";
+        } else {
+            $strasse = test_input($_POST["strasse"]);
+        }
+        if (empty($_POST["plz"])) {
+            $plzErr = "Plz is required";
+        } else {
+            $plz = test_input($_POST["plz"]);
+        }
+        if (empty($_POST["ort"])) {
+            $ortErr = "Ort is required";
+        } else {
+            $ort = test_input($_POST["ort"]);
+        }
+        $jahreszeit = $_POST["jahreszeit"];
+        $wuensche = $_POST["wuensche"];
+    }
+    //Check if input is valid
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    //connect to database
+
+    ?>
+    <!-- Formular -->
+    <form name="Infomaterial" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+        <h1>Infomaterial</h1>
+        <p><span class="error">* required field</span></p>
+        <p>Bitte senden Sie mir Infomaterial!</p>
+
+        <table class="infoTabelle">
+            <tr>
+                <td>
+                    <label for="i1"> Anrede:</label>
+                </td>
+                <td>
+                    <select name="info" id="i1">
+                        <option value="Familie">Familie</option>
+                        <option value="Frau">Frau</option>
+                        <option value="Herr">Herr</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="i2">Name:</label>
+                </td>
+                <td>
+
+                    <input type="text" id="i2" name="name" autofocus>
+                    <span class="error">* <?php echo $nameErr; ?></span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="i3">Strasse:</label>
+                </td>
+                <td>
+                    <input type="text" name="strasse" id="i3">
+                    <span class="error">* <?php echo $strasseErr; ?></span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="">Plz</label>
+                </td>
+                <td>
+                    <input type="text" name="plz" maxlength="5" style="width: 40px;" pattern="[0-9]+">
+                    <span class="error">* <?php echo $plzErr; ?></span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="">Ort:</label>
+                </td>
+                <td>
+                    <input type="text" name="ort" required>
+                    <span class="error">* <?php echo $ortErr; ?></span>
+                </td>
+            </tr>
+        </table>
+        <br>
+        <label>Ich beabsichtige einen Aufenthalt:</label>
+        <br> <br>
+
+        <table class="infoTabelle">
+            <tr>
+                <td>
+                    <input type="radio" name="jahreszeit" value="Sommer">Sommer
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="radio" name="jahreszeit" value="Winter"> Winter
+                </td>
+            </tr>
+        </table>
+
+        <br> <br>
+        <label for="t1">Ich habe folgende Wünsche:</label>
+        <br>
+        <textarea name="wuensche" id="t1" cols="28" rows="4"></textarea>
+        <br> <br>
+        <input type="submit" value="absenden" />
+        <input type="reset" value="Formular leeren">
+    </form>
+    <!-- Showing posted values-->
+    <p>Anrede: <?php echo empty($info) ? '' : $info; ?></p>
+    <p>Name: <?php echo empty($name) ? '' : $name; ?></p>
+    <p>Strasse: <?php echo empty($strasse) ? '' : $strasse; ?></p>
+    <p>PLZ: <?php echo empty($plz) ? '' : $plz; ?></p>
+    <p>Ort: <?php echo empty($ort) ? '' : $ort; ?></p>
+    <p>Ich beabsichtige einen Aufenthalt: <?php echo empty($jahreszeit) ? '' : $jahreszeit; ?></p>
+    <p>Ich habe folgende Wünsche: <?php echo empty($wuensche) ? '' : $wuensche; ?></p>
+
+</body>
+
+</html>
